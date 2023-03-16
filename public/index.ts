89437c495b6c4286da9d7ed6189d2adc2228cd3c
Sean Mena <seanmena13@gmail.com>
2023-03-15 21:07:39 -0600

    added routing

---
+++ public/index.ts
@@ -0,0 +1,1 @@
+document.getElementsByClassName("start-btn")
--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile("index.html", { root: "public" });
+});
+
+app.get("/Contact", (req, res) => {
+  res.sendFile("Contact.html", { root: "public" });
+});
+
+app.get("/Art", (req, res) => {
+  res.sendFile("Art.html", { root: "public" });
+});
+
+app.get("/Comics", (req, res) => {
+  res.sendFile("Comics.html", { root: "public" });
+});
+
+app.get("/About", (req, res) => {
+  res.sendFile("About.html", { root: "public" });
 });
 
 app.listen(port, () => {
